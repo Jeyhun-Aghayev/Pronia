51c5b77b5fbcb7e9afe127f50b411c5713f20f1d
Jeyhun-Aghayev <bd8ez2q6v@code.edu.az>
2023-11-23 01:25:23 +0400

    sad#

--- Pronia/GlobalUsing.cs
+++ Pronia/GlobalUsing.cs
@@ -1,5 +1,6 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Mvc;
 global using Pronia.ViewModels;
+global using Pronia.Helpers;
 global using Pronia.Models;
 global using Pronia.DAL;
--- Pronia/Models/Slider.cs
+++ Pronia/Models/Slider.cs
@@ -1,14 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pronia.Models
 {
     public class Slider
     {
         public int Id { get; set; }
-        [Required,StringLength(10,ErrorMessage ="Uzunluq max 10 olmalidir")]
+        [Required,StringLength(25,ErrorMessage ="Uzunluq max 10 olmalidir")]
         public string Title { get; set; }
         public string SubTitle { get; set; }
+        [StringLength(maximumLength: 100)]
         public string Description { get; set; }
-        public string ImgUrl { get; set; }
+        public string? ImgUrl { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
     }
 }
